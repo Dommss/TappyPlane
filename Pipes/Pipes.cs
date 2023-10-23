663310061409400af5537d254441c3b4981f7b60
Dommss <Dommterr@gmail.com>
2023-10-23 13:42:32 +0200

    Added scrolling and self-deleting pipes

---
+++ Pipes/Pipes.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class Pipes : Node2D {
+    private float _scrollSpeed = 50f;
+
+    private VisibleOnScreenNotifier2D _notifier;
+
+    public override void _Ready() {
+        _notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+        _notifier.ScreenExited += OnScreenExited;
+    }
+
+    public override void _Process(double delta) {
+        var position = Position;
+        position.X -= _scrollSpeed * (float)delta;
+        Position = position;
+    }
+
+    private void OnScreenExited() {
+        QueueFree();
+    }
+}
