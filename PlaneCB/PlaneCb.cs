020506a340a898c72e2af17a1aae1711aa772f73
Dommss <Dommterr@gmail.com>
2023-10-23 12:26:47 +0200

    Added a fly animation

--- PlaneCB/PlaneCb.cs
+++ PlaneCB/PlaneCb.cs
@@ -3,12 +3,23 @@ using Godot;
 public partial class PlaneCb : CharacterBody2D {
     private const float Gravity = 1900f;
     private const float Power = -400f;
+
+    private AnimationPlayer _animationPlayer;
+
+    public override void _Ready() {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
+
     public override void _PhysicsProcess(double delta) {
+        Fly(delta);
+}
+
+    private void Fly(double delta) {
         Vector2 velocity = Velocity;
         velocity.Y += Gravity * (float)delta;
-
         if (Input.IsActionJustPressed("Fly")) {
             velocity.Y = Power;
+            _animationPlayer.Play("Fly");
         }
 
         Velocity = velocity;
